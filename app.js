a5954af6b7b88d136e215e5b5210672ed81ce1bb
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-07-13 01:04:00 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+//SELECT ITEMS
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+//edit option 
+let editElement;
+let editFlag = false;
+let editID = "";
+
+//Event listeners
+//submit form
+form.addEventListener("submit", addItem);
+
+//Functions
+function addItem(e) {
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+    //if value is not empty and not editing
+    if(value !== '' && editFlag === false) {
+        console.log("add item to list");
+    }
+    //if value is not empty and editing
+    else if(value !== '' && editFlag === true) {
+        console.log("editing");
+    } else {
+        console.log("empty value");
+    }
+
+    console.log(id);
+}
